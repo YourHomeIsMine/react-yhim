542fa8e8899ea26f3e273721afc431aafa2578fc
hyejin30 <tyui3185@gmail.com>
2023-03-01 23:22:39 +0900

    Refactor: Change children to Outlet in Layout component

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from 'pages/Main';
 import SignUp from 'pages/SignUp';
+import Layout from 'components/layout/Layout';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Main />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
