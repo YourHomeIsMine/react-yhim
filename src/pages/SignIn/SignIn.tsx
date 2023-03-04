e3c947daf699fb94886d885c6c2ba7869e27bc39
tanonkim <robbins4bos@gmail.com>
2023-03-04 13:18:03 +0900

    Feature: Add text input

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,12 +1,43 @@
 import styled from 'styled-components';
-import { colors, flex, font } from '../../styles';
+import { colors, flex, font } from 'styles';
+import React, { useState } from 'react';
+import TextInput from '../SignUp/TextInput';
+import { useNavigate } from 'react-router-dom';
 
 const SignIn = () => {
+  const [signInList, setSignInList] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+    setSignInList({ ...signInList, [name]: value });
+  };
+
   return (
     <Container>
       <Header>회원가입 및 소셜로그인</Header>
       <Title>YHIM에 오신 것을 환영합니다.</Title>
-
+      <TextList>
+        <TextInput
+          label="이메일"
+          type="text"
+          placeholder="예) YourHomeIsMine@yhim.com"
+          className="email"
+          name="email"
+          onChange={handleChange}
+        />
+        <TextInput
+          label="비밀번호"
+          type="password"
+          placeholder="영문 대소문자, 숫자, 특수문자를 포함하여 8자리 이상"
+          className="password"
+          name="password"
+          onChange={handleChange}
+        />
+      </TextList>
+      <hr />
       <SignUpBtn>로그인</SignUpBtn>
     </Container>
   );
@@ -28,9 +59,18 @@ const Header = styled.header`
 
 const Title = styled.div`
   width: 100%;
-  padding: 20px;
+  padding: 20px 300px 20px 20px;
   margin-top: 10px;
   ${font(20, 500)};
+`;
+
+const TextList = styled.div`
+  display: grid;
+  grid-template-rows: 1fr 1fr;
+  align-items: center;
+  width: 100%;
+  padding: 10px 0px 10px;
+  margin-left: 20px;
 `;
 
 const SignUpBtn = styled.button`
